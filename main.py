dbe6db956e32c7b72853afe968ef0c327942e540
Ameglebm <amegleeu@gmail.com>
2024-06-19 00:00:30 -0300

    lista continua

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-amar,azul,acordado,amargura,assistir,apetecer,ausência,ascender,arrojado,aprender,alcançar,apêndice,atribuir,anarquia,amistoso,arretado,ajudante,atraente,apoteose,atrofiar,autônomo,adiantar,arrancar,afrontar,abestado,autópsia.
-
+lista = ["amar","azul","acordado","amargura","assistir",apetecer,ausência,ascender,arrojado,aprender,alcançar,apêndice,atribuir,anarquia,amistoso,arretado,ajudante,atraente,apoteose,atrofiar,autônomo,adiantar,arrancar,afrontar,abestado,autópsia.
 bizarro,bem,buscar,basculho,boçal,bastardo,bom,brado,bíblia,balbúrdia,brasileiro,burro,bruma,base,brega,bravo,bálsamo,bênção,bonito,bojo,barganha,beneficência,beldade,bondade,bola
-bajulador,belo,brasil,bolo,burocrático,burrice,bobo,bilíngue,bruços,bônus,biodiversidade.
-
+bajulador,belo,brasil,bolo,burocrático,burrice,bobo,bilíngue,bruços,bônus,biodiversidade
 cultura,confiança,como,conserto,circunstância,caos,consequência,casual,cinismo,cético,conveniência,compaixão,consonância,cordial,compartilhar,complexo,céu,clichê,critério,coragem,
 consoante,carência,canalha,coerente,custódia,colapso,casa,caridade,coincidência,conhecer
-complexidade,conforme.
-
+complexidade,conforme
 descobrir,defasagem,desalento,diplomata,democrata,divertido,destituir,dezesseis,discordar
 distraído,devolução,disfarçar,delegação,devastado,desonesto,dilapidar,desabafar,deposição
-dominação,denunciar,desejável,desbocado,devaneios,diabólico,despedida,desamparo,dilatação,descrença,desgastar,debandada,disjunção,desanimar,deliciosa,decrescer,dessagrar,desatacar.
+dominação,denunciar,desejável,desbocado,devaneios,diabólico,despedida,desamparo,dilatação,descrença,desgastar,debandada,disjunção,desanimar,deliciosa,decrescer,dessagrar,desatacar]
