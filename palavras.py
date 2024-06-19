6eeee21d9598ad2edcc0c7b050dbe28f49fcf4ee
almeidakah <karlaghale@gmail.com>
2024-06-19 00:48:15 -0300

    oo

--- palavras.py
+++ palavras.py
@@ -1,9 +1,11 @@
-lista = ["amar","azul","acordado","amargura","assistir","apetecer",ausência,ascender,arrojado,aprender,alcançar,apêndice,atribuir,anarquia,amistoso,arretado,ajudante,atraente,apoteose,atrofiar,autônomo,adiantar,arrancar,afrontar,abestado,autópsia.
-bizarro,bem,buscar,basculho,boçal,bastardo,bom,brado,bíblia,balbúrdia,brasileiro,burro,bruma,base,brega,bravo,bálsamo,bênção,bonito,bojo,barganha,beneficência,beldade,bondade,bola
-bajulador,belo,brasil,bolo,burocrático,burrice,bobo,bilíngue,bruços,bônus,biodiversidade
-cultura,confiança,como,conserto,circunstância,caos,consequência,casual,cinismo,cético,conveniência,compaixão,consonância,cordial,compartilhar,complexo,céu,clichê,critério,coragem,
-consoante,carência,canalha,coerente,custódia,colapso,casa,caridade,coincidência,conhecer
-complexidade,conforme
-descobrir,defasagem,desalento,diplomata,democrata,divertido,destituir,dezesseis,discordar
-distraído,devolução,disfarçar,delegação,devastado,desonesto,dilapidar,desabafar,deposição
-dominação,denunciar,desejável,desbocado,devaneios,diabólico,despedida,desamparo,dilatação,descrença,desgastar,debandada,disjunção,desanimar,deliciosa,decrescer,dessagrar,desatacar]
+lista = ["amar","azul","acordado","amargura","assistir","apetecer","ascender","arrojado","aprender","alcançar","atribuir","anarquia","amistoso","arretado","ajudante","atraente","apoteose","atrofiar","adiantar","arrancar","afrontar","abestado"
+"bizarro","bem","buscar","basculho","bastardo","bom","brado","brasileiro","burro","bruma","base","brega","bravo","bonito","bojo","barganha","beldade","bondade","bola"
+"bajulador","belo","brasil","bolo","burrice","bobo","bruços","bônus","biodiversidade"
+"cultura","confiança","como","conserto","circunstância","caos","consequência","casual","cinismo","cético","conveniência","compaixão","consonância","cordial","compartilhar","complexo","céu","clichê","critério","coragem",
+"consoante","carência","canalha","coerente","custódia","colapso","casa","caridade","conhecer","complexidade","conforme"
+"descobrir","defasagem","desalento","diplomata","democrata","divertido","destituir","dezesseis","discordar","distraído","devolução","disfarçar","devastado","desonesto","dilapidar","desabafar"
+"denunciar","desbocado","devaneios","despedida","desamparo","descrença","desgastar","debandada","desanimar","deliciosa","decrescer","dessagrar","desatacar"
+"elo", "entusiasta", "estima", "emissão","extorquir","esboço", "extinguir","explicar", "errante","exalar","executar", "exclusão", "elegibilidade","erudito", "eficaz", "estúpido","expedido","exótico", "estereótipo"
+"fragmentado", "fascinante","flanco","factual","fragrância","fito","férias", "fiel"
+"funcionalidade", "feminino","fechar","fascínio","fatores","franqueza" "fofoca","fugir"
+"fenda", "fustigado", "fascinar","fraternal", "flor", "fundamentar", "ficha"]
